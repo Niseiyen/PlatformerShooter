6e273c17ceebba60be709c6c6ca5e05defcc7826
Niseiyen <127323783+Niseiyen@users.noreply.github.com>
2024-11-12 07:11:35 -0500

    Gun

--- platformeshooter/Acteur/Player/Script/bullet.gd
+++ platformeshooter/Acteur/Player/Script/bullet.gd
@@ -1,1 +1,9 @@
 extends Area2D
+
+@export var speed = 400
+
+func _process(delta):
+	position += Vector2(cos(rotation), sin(rotation)) * speed * delta
+	
+	if not get_viewport_rect().encloses(Rect2(global_position, Vector2(1, 1))):
+		queue_free()
--- platformeshooter/Acteur/Player/Script/gun.gd
+++ platformeshooter/Acteur/Player/Script/gun.gd
@@ -1,8 +1,21 @@
 extends Sprite2D
 
+const BULLET = preload("res://Acteur/Player/Bullet.tscn")
+@onready var marker_2d: Marker2D = $Marker2D
+
 func _process(delta):
+	# Rotation de l'arme vers la souris
 	var mouse_position = get_global_mouse_position()
-	
 	var direction = (mouse_position - global_position).normalized()
-	
 	rotation = direction.angle()
+	
+	# Détection du clic gauche pour tirer une balle
+	if Input.is_action_just_pressed("shoot"):
+		shoot()
+
+func shoot():
+	# Instance de la balle et ajout à la scène
+	var bullet_instance = BULLET.instantiate()
+	bullet_instance.position = marker_2d.global_position  
+	bullet_instance.rotation = rotation
+	get_parent().add_child(bullet_instance)
